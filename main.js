801e58f6950c4df0c38dc81f60ce04dd169d1ebb
Artem Myrhorod <mirgorodartem@gmail.com>
2023-01-20 20:11:13 +0100

    Solved Casino play task

--- main.js
+++ main.js
@@ -134,3 +134,33 @@ function sequentialSizes(val) {
 }
 
 sequentialSizes(1);
+
+
+//Casino play
+
+let count = 0;
+
+function cc(card) {
+   switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+         count++;
+         break;
+      case 10:
+      case 'J':
+      case 'Q':
+      case 'K':
+      case 'A':
+         count--;
+         break;
+   }
+   let holdBet = 'Hold';
+   if (count > 0) {
+      holdBet = 'Bet';
+   }
+   return count + ' ' + holdBet;
+
+}
