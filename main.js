73000158e13be401b79180551e71a2ef8f76172a
Artem Myrhorod <mirgorodartem@gmail.com>
2023-01-20 17:03:30 +0100

    Write Switch Case

--- main.js
+++ main.js
@@ -85,3 +85,26 @@ function golfScore(par, strokes) {
 }
 
 console.log(golfScore(5, 4));
+
+// Switch case
+
+function switchOfStuff(val) {
+   let answer = "";
+   switch (val) {
+      case 'a':
+         answer = 'apple';
+         break;
+      case 'b':
+         answer = 'bird';
+         break;
+      case 'c':
+         answer = 'cat';
+         break;
+      default:
+         answer = 'stuff';
+         break;
+   }
+   return answer;
+}
+
+console.log(switchOfStuff(1));
