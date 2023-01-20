6146f9d9191ea38d27b09e4b8e1c54af9c10b8a5
Artem Myrhorod <mirgorodartem@gmail.com>
2023-01-20 16:48:40 +0100

    Write switch case

--- main.js
+++ main.js
@@ -85,3 +85,25 @@ function golfScore(par, strokes) {
 }
 
 golfScore(5, 4);
+
+// Switch case construction
+
+function caseInSwitch(val) {
+   let answer = "";
+   switch (val) {
+      case 1:
+         answer = 'alpha';
+         break;
+      case 2:
+         answer = 'beta';
+         break;
+      case 3:
+         answer = 'gamma';
+         break;
+      case 4:
+         answer = 'delta';
+         break;
+   }
+   return answer;
+}
+console.log(caseInSwitch(2));
