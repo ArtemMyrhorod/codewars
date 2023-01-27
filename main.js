16ca8c4bf23ade351f0212c1da88f5e26559ae5a
Artem Myrhorod <mirgorodartem@gmail.com>
2023-01-27 05:30:24 +0100

    Solved Grasshopper - Summation Task

--- main.js
+++ main.js
@@ -433,3 +433,13 @@ class SmallestIntegerFinder {
       return result;
    }
 }
+
+// Grasshopper - Summation
+
+const summation = function (num) {
+   let sum = 0;
+   for (let i = 1; i <= num; i++) {
+      sum = sum + i;
+   }
+   return sum;
+};
