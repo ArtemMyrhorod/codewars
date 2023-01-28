07317a471ab368780cc44ae5f01db251fa846f00
Artem Myrhorod <mirgorodartem@gmail.com>
2023-01-28 17:58:15 +0100

    Solved Cat years, Dog years task

--- main.js
+++ main.js
@@ -544,3 +544,21 @@ function century(year) {
    let result = Math.ceil(year / 100);
    return result;
 }
+
+//Cat years, Dog years
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+   let catYears;
+   let dogYears;
+   if (humanYears === 1) {
+      catYears = 15;
+      dogYears = 15;
+   } else if (humanYears === 2) {
+      catYears = 24;
+      dogYears = 24;
+   } else {
+      catYears = 24 + 4 * (humanYears - 2);
+      dogYears = 24 + 5 * (humanYears - 2);
+   }
+   return [humanYears, catYears, dogYears];
+};
