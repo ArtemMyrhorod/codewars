236107675be465c2783c15588af0362e921e6f02
Artem Myrhorod <mirgorodartem@gmail.com>
2023-01-29 15:12:20 +0100

    Add all numbers in array

--- main.js
+++ main.js
@@ -596,3 +596,10 @@ function mainFunction(randomNumber, shouldCall, callback) {
    return result;
 }
 console.log(mainFunction(20, true, myCallBack));
+
+// Add all numbers in array
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+for (let i = 0; i < myArr.length; i++) {
+   total += myArr[i];
+}
